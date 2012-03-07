04e6dac4ba92a97b7344c2999db9b417499e289f
Adam Farden <adam@farden.cz>
2012-03-07 14:34:38 +0100

    Add common config for devices with low space on system
    Change-Id: I4e63dc4d608ed79c36c6757b2ffbfc76cd3634e6

--- config/common.mk
+++ config/common.mk
@@ -44,8 +44,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so \
     vendor/cm/prebuilt/common/bootanimation.zip:system/media/bootanimation.zip
 
-# Bring in camera effects & videos
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
+# Bring in camera effects
 PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/cm/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -2,14 +2,7 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all audio files
-#include frameworks/base/data/sounds/AllAudio.mk
 include frameworks/base/data/sounds/NewAudio.mk
-
-# Extra Ringtones
-include frameworks/base/data/sounds/NewWave.mk
-
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
