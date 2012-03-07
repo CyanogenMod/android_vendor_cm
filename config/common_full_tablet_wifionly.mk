321dde5f48a8af75c220ffe26eb389f4c1255385
Adam Farden <adam@farden.cz>
2012-03-07 14:34:38 +0100

    Add common config for devices with low space on system
    Change-Id: I4e63dc4d608ed79c36c6757b2ffbfc76cd3634e6

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -2,7 +2,10 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/AllAudio.mk
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Extra Ringtones
+include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -4,12 +4,6 @@ $(call inherit-product, vendor/cm/config/common.mk)
 # Bring in all audio files
 include frameworks/base/data/sounds/NewAudio.mk
 
-# Extra Ringtones
-include frameworks/base/data/sounds/AudioPackageNewWave.mk
-
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
@@ -24,5 +18,5 @@ PRODUCT_PACKAGES += \
 
 ifeq ($(TARGET_BOOTANIMATION_NAME),)
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/vertical-480x800.zip:system/media/bootanimation.zip
+        vendor/cm/prebuilt/common/bootanimation/vertical-320x480.zip:system/media/bootanimation.zip
 endif
