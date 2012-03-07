70edbfaf3738441d78cc9b77987326488c46c4d8
Adam Farden <adam@farden.cz>
2012-03-07 14:34:38 +0100

    Add common config for devices with low space on system
    Change-Id: I4e63dc4d608ed79c36c6757b2ffbfc76cd3634e6

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common.mk)
 
 # Bring in all audio files
-include frameworks/base/data/sounds/AllAudio.mk
+include frameworks/base/data/sounds/NewAudio.mk
 
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
