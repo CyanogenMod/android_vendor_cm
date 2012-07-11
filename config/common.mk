99795ae057ee61a91ea743b15bb46b3521c20b41
netchip <vanderpoel24@gmail.com>
2012-07-11 10:05:29 +0200

    Make ready for CM10
    Change-Id: I329307fe6ec3f37a2fbe7ca0744c80e172dde594
    Signed-off-by: netchip <vanderpoel24@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
 # T-Mobile theme engine
-include vendor/cm/config/themes_common.mk
+#include vendor/cm/config/themes_common.mk
 
 # Required CM packages
 PRODUCT_PACKAGES += \
@@ -119,9 +119,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
-PRODUCT_VERSION_MAJOR = 9
+PRODUCT_VERSION_MAJOR = 10
 PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = 0-RC1
+PRODUCT_VERSION_MAINTENANCE = 0-RC0
 
 # Set CM_BUILDTYPE
 ifdef CM_NIGHTLY
