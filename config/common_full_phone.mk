7a1e5a37687e02d4f664f404b6d8ee092135066a
Derek Sevilla <deryk0917@gmail.com>
2012-07-24 21:49:33 -0700

    Remove videos from non-tuna devices
    Tuna devices just need to add removed lines into device tress
    
    Change-Id: Iad5153c9998e3b6e281f6845034b9abf7d40116d

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -7,9 +7,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # Extra Ringtones
 include frameworks/base/data/sounds/AudioPackageNewWave.mk
 
-# Bring in all video files
-$(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
-
 # Include CM audio files
 include vendor/cm/config/cm_audio.mk
 
