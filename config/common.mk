e4ff7a1a53a06eeb1799ad492dae5bb760634f34
Steve Kondik <shade@chemlab.org>
2012-08-09 19:23:58 -0700

    cm: Use prebuilt Superuser
     * Temporary APK until ChainsDD fixes the ActionBarSherlock issue
    
    Change-Id: I29ad308b562884c0f2981dc08290070f4ec99364

--- config/common.mk
+++ config/common.mk
@@ -53,7 +53,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/RomManager.apk:system/app/RomManager.apk \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
+	vendor/cm/prebuilt/common/apps/Superuser.apk:system/app/Superuser.apk
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
@@ -81,8 +82,6 @@ PRODUCT_PACKAGES += \
     Development \
     LatinIME \
     SpareParts \
-    Superuser \
-    Superuser.apk \
     su
 
 # Optional CM packages
