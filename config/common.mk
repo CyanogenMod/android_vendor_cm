c4eb00408447c269ad607c44bfb64a36d8ebb85e
Chirayu Desai <cdesai@cyanogenmod.org>
2013-02-22 20:21:05 +0530

    config: move ADB authentication prop to build.prop
    Change-Id: I8fd720c410b2e6297336dfb88b6fbe98868829ee

--- config/common.mk
+++ config/common.mk
@@ -67,7 +67,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
 
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+PRODUCT_PROPERTY_OVERRIDES += ro.adb.secure=1
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
