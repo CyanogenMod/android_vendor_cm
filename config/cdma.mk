4cfbf8a97de8e20f90e93809e52177e88336500d
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-22 20:42:39 -0500

    apns: consolidate gsm and cdma apns
    Change-Id: I702f3d7c7a5a7e15b5bf8b2e8bddd5e3c71a9ad4

--- config/cdma.mk
+++ config/cdma.mk
@@ -1,3 +1,3 @@
 # CDMA APN list
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml
+    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
