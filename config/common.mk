7a4a9724bad06c3dde318ee4d614c5738c43914a
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-12-10 04:35:08 -0800

    Add CM Legal (1 of 2)
    Allow defining a ro.cmlegal.url to for T&C and Privacy Policy
    
    Change-Id: I45ffbdd9e472e258ed8c5a63dff0f3b9c7bc04e2
    (cherry picked from commit 381a305718e1a34b1ef447faadef6ad6bd77736a)

--- config/common.mk
+++ config/common.mk
@@ -306,7 +306,8 @@ endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.version=$(CM_VERSION) \
-  ro.modversion=$(CM_VERSION)
+  ro.modversion=$(CM_VERSION) \
+  ro.cmlegal.url=http://www.cyanogenmod.org/docs/privacy
 
 -include vendor/cm/sepolicy/sepolicy.mk
 
