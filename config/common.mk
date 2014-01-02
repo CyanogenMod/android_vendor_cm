060a47e3327ede0775b7f38a6af330ba8a8a8716
mrimp <mrimp@day-off.net>
2014-01-02 15:55:10 -0500

    updates

--- config/common.mk
+++ config/common.mk
@@ -32,14 +32,6 @@ $(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size
 PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
 endif
 
-ifdef CM_NIGHTLY
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmodnightly
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=cyanogenmod
-endif
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -149,6 +141,7 @@ PRODUCT_PACKAGES += \
     CMUpdater \
     CMFota \
     CMAccount \
+    HALO \
     WhisperPush
 
 # CM Hardware Abstraction Framework
@@ -225,6 +218,10 @@ endif
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
+PRODUCT_VERSION_MAJOR = 3
+PRODUCT_VERSION_MINOR = 3.1
+PRODUCT_VERSION_MAINTENANCE = OFFICIAL
+
 # Set CM_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
 
 ifndef CM_BUILDTYPE
@@ -235,42 +232,57 @@ ifndef CM_BUILDTYPE
     endif
 endif
 
-ifdef BUILDTYPE_NIGHTLY
-        CM_BUILDTYPE := NIGHTLY
-endif
-ifdef BUILDTYPE_AUTOTEST
-        CM_BUILDTYPE := AUTOTEST
-endif
-ifdef BUILDTYPE_EXPERIMENTAL
-        CM_BUILDTYPE := EXPERIMENTAL
-endif
-ifdef BUILDTYPE_RELEASE
-        CM_BUILDTYPE := RELEASE
+# Filter out random types, so it'll reset to UNOFFICIAL
+ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+    CM_BUILDTYPE :=
 endif
 
-ifndef CM_BUILDTYPE
-        CM_BUILDTYPE := UNOFFICIAL
-endif
-
-TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
-TARGET_PRODUCT_SHORT := $(subst omni_,,$(TARGET_PRODUCT_SHORT))
-
-# Build the final version string
-ifdef BUILDTYPE_RELEASE
-        CM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
+ifdef CM_BUILDTYPE
+    ifneq ($(CM_BUILDTYPE), SNAPSHOT)
+        ifdef CM_EXTRAVERSION
+            # Force build type to EXPERIMENTAL
+            CM_BUILDTYPE := EXPERIMENTAL
+            # Remove leading dash from CM_EXTRAVERSION
+            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
+            # Add leading dash to CM_EXTRAVERSION
+            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
+        endif
+    else
+        ifndef CM_EXTRAVERSION
+            # Force build type to EXPERIMENTAL, SNAPSHOT mandates a tag
+            CM_BUILDTYPE := EXPERIMENTAL
+        else
+            # Remove leading dash from CM_EXTRAVERSION
+            CM_EXTRAVERSION := $(shell echo $(CM_EXTRAVERSION) | sed 's/-//')
+            # Add leading dash to CM_EXTRAVERSION
+            CM_EXTRAVERSION := -$(CM_EXTRAVERSION)
+        endif
+    endif
 else
-ifeq ($(CM_BUILDTIME_LOCAL),y)
-        CM_VERSION := IOAP-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)-$(CM_BUILD)
-else
-        CM_VERSION := IOAP-$(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)-$(CM_BUILD)
-endif
+    # If CM_BUILDTYPE is not defined, set to UNOFFICIAL
+    CM_BUILDTYPE := UNOFFICIAL
+    CM_EXTRAVERSION :=
 endif
 
+ifeq ($(CM_BUILDTYPE), UNOFFICIAL)
+    ifneq ($(TARGET_UNOFFICIAL_BUILD_ID),)
+        CM_EXTRAVERSION := -$(TARGET_UNOFFICIAL_BUILD_ID)
+    endif
+endif
+
+ifeq ($(CM_BUILDTYPE), RELEASE)
+    CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR).$(PRODUCT_VERSION_MAINTENANCE)$(PRODUCT_VERSION_DEVICE_SPECIFIC)-$(shell date -u +%Y%m%d)-$(CM_BUILD)
+else
+    ifeq ($(PRODUCT_VERSION_MINOR),0)
+        CM_VERSION := $(PRODUCT_VERSION_MAJOR)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)$(CM_EXTRAVERSION)-$(CM_BUILD)
+    else
+        CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)$(CM_EXTRAVERSION)-$(CM_BUILD)
+    endif
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.ioap.version=$(CM_VERSION) \
   ro.modversion=$(CM_VERSION) \
-  ro.cmlegal.url=http://www.cyanogenmod.org/docs/privacy
 
 -include vendor/cm-priv/keys/keys.mk
 
