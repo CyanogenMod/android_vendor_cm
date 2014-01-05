ae551b3e4cf8306d0c97096d6caee5e2a0d766a9
Steven Luo <steven@steven676.net>
2014-01-04 17:09:19 -0800

    Remove some optional packages from "mini" builds
    Change-Id: I31f502a10d64343c8800d51df28b6de49e44bccc

--- config/telephony.mk
+++ config/telephony.mk
@@ -12,3 +12,7 @@ PRODUCT_PACKAGES += \
     Stk \
     CellBroadcastReceiver \
     WhisperPush
+
+# Mms depends on SoundRecorder for recorded audio messages
+PRODUCT_PACKAGES += \
+    SoundRecorder
