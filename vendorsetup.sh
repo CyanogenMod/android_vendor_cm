bce51cbd1ff246832f11a3a7e288e3219fbf8c29
Dorian Snyder <dastin1015@gmail.com>
2014-01-11 16:37:09 -0800

    apn: modify Virgin Mobile US and Boost Mobile APN settings
    Change-Id: I5acb5b1ce2a729e3074721d16a6aa5f32800773f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
 do
+    add_lunch_combo cm_d710-userdebug
+    add_lunch_combo cm_m7spr-userdebug
     add_lunch_combo $combo
 done
