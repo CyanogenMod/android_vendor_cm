98758b48c593c20ed3a8ec37e3e8667bd0bcae95
Steve Kondik <shade@chemlab.org>
2014-05-11 01:41:31 -0700

    cm: Add a few dependencies to the build
     * Stuff needed by IZat and ipv6 tethering
    
    Change-Id: Ic44d7d0760755c793cc36fe605c9465ab7039b84

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -11,4 +11,11 @@ ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
         vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip
 endif
 
+# A few dependencies not tracked elsewhere
+PRODUCT_PACKAGES += \
+    curl \
+    ebtables \
+	libbson \
+	libnl_2
+
 $(call inherit-product, vendor/cm/config/telephony.mk)
