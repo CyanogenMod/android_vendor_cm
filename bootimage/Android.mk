chirayudesai/scaling-octo-batman

Add makefile to allow quick building of bootimage
* It's faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaast!

bootimage/Android.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2014 Chirayu Desai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is a quick hack to allow building a bootimage quickly without
+# needing to parse all the makefiles in the tree.
+# 'mmm mka vendor/cde/bootimage'
+# Is MUCH, MUCH faster than 'mka bootimage'
+
+# Guard so that it isn't included in builds not invoked via mm/mmm
+ifneq ($(ONE_SHOT_MAKEFILE),)
+ALL_MODULES += bootimage
+endif

CyanogenMod/android_vendor_cm

Change-Id: Ia27e17630a780d51909e4a83109f79a297290524
(cherry picked from commit 8e0b6602df5a31ddf67f13775882ea93a0df6f8b)

bootimage/Android.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This is a quick hack to allow building a bootimage quickly without
+# needing to parse all the makefiles in the tree.
+# 'mmm mka vendor/cde/bootimage'
+# Is MUCH, MUCH faster than 'mka bootimage'
+
+# Guard so that it isn't included in builds not invoked via mm/mmm
+ifneq ($(ONE_SHOT_MAKEFILE),)
+ALL_MODULES += kernel
+endif