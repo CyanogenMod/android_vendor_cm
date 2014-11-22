68ada54edcdb88875e9c026f3c23613e03a2f346
Shaun Eaton <shaunsibu@gmail.com>
2014-11-22 11:43:44 -0700

    Stop DSP From building
    Change-Id: I248354ca04337dd5391d6446b0f6dbcfc0517eda

--- config/common.mk
+++ config/common.mk
@@ -144,8 +144,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Launcher3 \
     Trebuchet \
-    DSPManager \
-    libcyanogen-dsp \
     audio_effects.conf \
     CMWallpapers \
     Apollo \
