1adac161fddb0b4cb77197b2f08013d81dd387d3
Patrick <pjgraber03@blissroms.com>
2014-11-29 05:46:04 +0100

    Remove unneeded path from commit

--- configs/common.mk
+++
@@ -1,6 +0,0 @@
-# SuperSU
-PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/bliss/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
-
-
