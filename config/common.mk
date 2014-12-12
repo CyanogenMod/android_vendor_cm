54f38030ffc75d27b50212c47cb9aa08edeb929e
Tom Marshall <tdm@cyngn.com>
2014-12-12 11:56:04 -0800

    cm: Put install tools in OUT/install
     * This allows us to avoid modifying /system at install time.
    
    Change-Id: I747551d7f38a3eef70ab64f32b6d4a4749c01012

--- config/common.mk
+++ config/common.mk
@@ -73,15 +73,15 @@ PRODUCT_COPY_FILES += \
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/bliss/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/bliss/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/bliss/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/bliss/prebuilt/common/bin/50-bliss.sh:system/addon.d/50-bliss.sh \
     vendor/bliss/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 endif
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
-    vendor/bliss/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+    vendor/bliss/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
 # init.d support
 PRODUCT_COPY_FILES += \
