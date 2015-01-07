a74ba3936bdb0b846a42171324115995ec27542d
Shaun Eaton <shaunsibu@gmail.com>
2015-01-07 09:58:25 -0700

    Changes in checking in Major or Minor versions.
    Change-Id: I5819b8b5e252d4346495d0c2093c46c525ae2e55

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -22,10 +22,7 @@ restore_addon_d() {
 
 # Proceed only if /system is the expected major and minor version
 check_prereq() {
-if ( ! grep -q "^ro.bliss.version=$V-*" /system/build.prop ); then
-  echo "Not backing up files from incompatible version: $V"
-  return 0
-fi
+echo "$V"
 return 1
 }
 
