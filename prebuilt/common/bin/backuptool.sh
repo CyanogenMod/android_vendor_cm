6373373311d0b236e59f9a505f877cd377e515f4
Shaun Eaton <shaunsibu@gmail.com>
2015-01-12 10:58:37 -0700

    Revert recent changes that are effecting legacy devices.
    Change-Id: Id2756c2c47e5457e6e059e577b245d83cdc415ed

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=BlissPop
+export V=12
 
 # Preserve /system/addon.d in /tmp/addon.d
 preserve_addon_d() {
@@ -22,7 +22,10 @@ restore_addon_d() {
 
 # Proceed only if /system is the expected major and minor version
 check_prereq() {
-echo "$V"
+if ( ! grep -q "^ro.bliss.version=$V.*" /system/build.prop ); then
+  echo "Not backing up files from incompatible version: $V"
+  return 0
+fi
 return 1
 }
 
