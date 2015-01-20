b315d9952af12f579e6e97d66ea3e5437aedfe4b
Shaun Eaton <shaunsibu@gmail.com>
2015-01-20 01:13:56 -0700

    Lets use the changelog generator.
    Change-Id: Ic3cc62f57fd98a40a35faa549e62dc5be5d54c03

--- config/common.mk
+++ config/common.mk
@@ -66,10 +66,6 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
-# Copy over the changelog to the device
-PRODUCT_COPY_FILES += \
-    vendor/bliss/CHANGELOG.mkdn:system/etc/CHANGELOG-bliss.txt
-
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
