580c854264f9794cd152fca6e047593fa2955704
Adnan Begovic <adnan@cyngn.com>
2015-05-19 14:10:50 -0700

    CM: Provide droiddoc cmsdk template.
    Change-Id: I3825b2a1dbd680d25c28bfad49c71b94119dcd8f

---
+++ build/tools/droiddoc/templates-cmsdk/assets/navtree_data.js
@@ -0,0 +1,15 @@
+var NAVTREE_DATA =
+[ [ "org.cyanogenmod.launcher.home.api.cards", "org/cyanogenmod/launcher/home/api/cards/package-summary.html", [ [ "Classes", null, [ [ "CardData", "org/cyanogenmod/launcher/home/api/cards/CardData.html", null, "" ], [ "CardData.CardDataIntentInfo", "org/cyanogenmod/launcher/home/api/cards/CardData.CardDataIntentInfo.html", null, "" ], [ "CardData.CardDeletedInfo", "org/cyanogenmod/launcher/home/api/cards/CardData.CardDeletedInfo.html", null, "" ], [ "CardDataImage", "org/cyanogenmod/launcher/home/api/cards/CardDataImage.html", null, "" ], [ "PublishableCard", "org/cyanogenmod/launcher/home/api/cards/PublishableCard.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "CardData.Priority", "org/cyanogenmod/launcher/home/api/cards/CardData.Priority.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.cyanogenmod.launcher.home.api.db", "org/cyanogenmod/launcher/home/api/db/package-summary.html", [ [ "Classes", null, [ [ "CmHomeDatabaseHelper", "org/cyanogenmod/launcher/home/api/db/CmHomeDatabaseHelper.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.cyanogenmod.launcher.home.api.provider", "org/cyanogenmod/launcher/home/api/provider/package-summary.html", [ [ "Interfaces", null, [ [ "CmHomeContract.ICmHomeContract", "org/cyanogenmod/launcher/home/api/provider/CmHomeContract.ICmHomeContract.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "CmHomeContentProvider", "org/cyanogenmod/launcher/home/api/provider/CmHomeContentProvider.html", null, "" ], [ "CmHomeContract", "org/cyanogenmod/launcher/home/api/provider/CmHomeContract.html", null, "" ], [ "CmHomeContract.CardDataContract", "org/cyanogenmod/launcher/home/api/provider/CmHomeContract.CardDataContract.html", null, "" ], [ "CmHomeContract.CardDataImageContract", "org/cyanogenmod/launcher/home/api/provider/CmHomeContract.CardDataImageContract.html", null, "" ], [ "CmHomeContract.ImageFile", "org/cyanogenmod/launcher/home/api/provider/CmHomeContract.ImageFile.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.cyanogenmod.launcher.home.api.receiver", "org/cyanogenmod/launcher/home/api/receiver/package-summary.html", [ [ "Classes", null, [ [ "CmHomeCardChangeReceiver", "org/cyanogenmod/launcher/home/api/receiver/CmHomeCardChangeReceiver.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
