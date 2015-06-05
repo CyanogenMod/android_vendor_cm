ed7ca31fcb2d08b0efabaae4dd824caa5f7d3703
Adnan Begovic <adnan@cyngn.com>
2015-06-04 19:23:35 -0700

    vendor/cm: Create CM_PLATFORM_SDK_VERSION.
    Change-Id: I7126b1ac93fbe3e0d5542279cb1f96dfc214bb23

---
+++ build/core/version_defaults.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Handle various build version information.
+#
+# Guarantees that the following are defined:
+#     CM_PLATFORM_SDK_VERSION
+#
+
+ifeq "" "$(CM_PLATFORM_SDK_VERSION)"
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 1
+endif
