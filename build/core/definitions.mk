477342f4b085e424b13bf6f654aa40b84f94804f
Chirayu Desai <chirayudesai1@gmail.com>
2015-06-11 00:44:51 +0530

    Add macros to generate the proprietary file list
    * Called with a list of modules, it returns
      with all of the installed files for the module,
      and the 32 bit variant of the module too if present.
    * Also gets the proprietary dependencies of those modules.
    * Paths returned are relative to system,
      suitable for our proprietary-files.txt and scripts.
    
    Change-Id: Iabc39a14df89f6953f3121d294d9a97b32613f6a

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -4,3 +4,38 @@
 define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
+
+# Resolve the required module name to 32-bit or 64-bit variant.
+# Get a list of corresponding 32-bit module names, if one exists.
+# From build/core/main.mk
+define get-32-bit-modules
+$(strip $(foreach m,$(1),\
+  $(if $(ALL_MODULES.$(m)$(TARGET_2ND_ARCH_MODULE_SUFFIX).CLASS),\
+    $(m)$(TARGET_2ND_ARCH_MODULE_SUFFIX))))
+endef
+
+define get-proprietary-dependencies-path
+$(strip $(foreach m,$(1),\
+  $(foreach p,$(ALL_MODULES.$(m).REQUIRED),\
+    $(foreach mk,$(ALL_MODULES.$(p).MAKEFILE),\
+      $(if $(filter $(2)/%,$(mk)),\
+        $(p))))))
+endef
+
+define get-proprietary-dependencies
+$(call get-proprietary-dependencies-path,$(1),vendor)
+endef
+
+# Generate the list of proprietary files installed for the required modules
+define get-proprietary-files-list
+$(eval p_p := $(1))\
+$(eval p_p += $(call get-32-bit-modules,$(1)))\
+$(eval p_p_d := $(call get-proprietary-dependencies,$(p_p)))\
+$(eval p :=)\
+$(foreach _p,$(call uniq,$(p_p) $(p_p_d)),\
+  $(foreach mk,$(ALL_MODULES.$(_p).MAKEFILE),\
+    $(if $(filter vendor/%,$(mk)),\
+       $(eval p += $(_p)))))\
+$(strip $(patsubst $(TARGET_OUT)/%,%,\
+  $(filter $(TARGET_OUT)/%,$(call module-installed-files,$(p)))))
+endef
