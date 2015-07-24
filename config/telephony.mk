82893b7abdf1958acbb7ca1b8c226ca9e8129af9
emancebo <emancebo@cyngn.com>
2015-07-23 17:17:34 -0700

    Add Smartfren SPN override
    The spn coming from CSIM for smartfren sims reads "smartfren" instead
    of "Smartfren".  Fix by overriding here.
    
    issue-id: RENDANG-421
    Change-Id: Iddd7e4c22fba15c3749dd9195b8961dc799e8fc5

--- config/telephony.mk
+++ config/telephony.mk
@@ -4,7 +4,8 @@ PRODUCT_COPY_FILES += \
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/cm/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml \
+    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
