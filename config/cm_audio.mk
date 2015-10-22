4bb976b0f73f5abbd0511ff1e78ccb96d7eb2b8d
Konsta <konsta09@gmail.com>
2015-10-22 21:37:06 +0300

    cm: this joke isn't funny anymore
    Remove remnants of april fools prank a few years back.
    
    This reverts commit bd3a941634a8b60cd4ac87fafd2d29cf09a5d0bd
    
    Change-Id: I3e97dfe531526aeb0f4cfb9362af2ab7a2b4b044

--- config/cm_audio.mk
+++ config/cm_audio.mk
@@ -29,7 +29,5 @@ PRODUCT_COPY_FILES += \
 	$(RINGTONE_PATH)/CyanTone.ogg:system/media/audio/ringtones/CyanTone.ogg \
 	$(RINGTONE_PATH)/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
 	$(RINGTONE_PATH)/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
-	$(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
-	$(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3
-
+	$(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg
 
