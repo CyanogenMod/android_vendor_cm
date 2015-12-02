24d6432ddb103643dad5fa9d656163b4c04db057
Tom Marshall <tdm@cyngn.com>
2015-12-02 13:24:54 -0800

    otasigcheck: Rewrite for CM 13.0
    Rewrite the packages.xml parser to be slightly less brittle.  Read
    lines from packages.xml and save off the interesting values, then
    compare with expected results afterward.
    
    This both avoids using grep's -A option, which is not supported in
    toybox, and allows the script to recognize the cert tag regardless
    of its position inside the package tag.
    
    Change-Id: Idc0006e38f4a3f9d5aec223a8a1571f5c11fe3bb

--- prebuilt/common/bin/otasigcheck.sh
+++ prebuilt/common/bin/otasigcheck.sh
@@ -15,14 +15,44 @@ if [ $? -eq 0 ]; then
   exit 0
 fi
 
-if [ -f /data/system/packages.xml -a -f /tmp/releasekey ]; then
-  relCert=$(grep -A3 'package name="com.android.htmlviewer"' /data/system/packages.xml  | grep "cert index" | head -n 1 | sed -e 's|.*"\([[:digit:]][[:digit:]]*\)".*|\1|g')
+if [ -f "/data/system/packages.xml" -a -f "/tmp/releasekey" ]; then
+  relkey=$(cat "/tmp/releasekey")
+  OLDIFS="$IFS"
+  IFS=""
+  while read line; do
+    echo "$line" | grep -q "^ *<cert index=[^ ]* key="
+    if [ $? -eq 0 ]; then
+      idx=$(echo $line | grep -o "index=[^ ]*" | cut -d'=' -f2 | cut -d'"' -f2)
+      key=$(echo $line | grep -o "key=[^ ]*" | cut -d'=' -f2 | cut -d'"' -f2)
+      eval "key_$idx=$key"
+      continue
+    fi
+    echo "$line" | grep -q "^ *<package name="
+    if [ $? -eq 0 ]; then
+      package=$(echo $line | grep -o "name=[^ ]*" | cut -d'=' -f2 | cut -d'"' -f2)
+      continue
+    fi
+    if [ "$package" != "com.android.htmlviewer" ]; then
+      continue
+    fi
+    echo $line | grep -q " *<cert index="
+    if [ $? -eq 0 ]; then
+      cert_idx=$(echo $line | grep -o "index=[^ ]*" | cut -d'=' -f2 | cut -d'"' -f2)
+      break
+    fi
+  done < "/data/system/packages.xml"
+  IFS="$OLDIFS"
 
   # Tools missing? Err on the side of caution and exit cleanly
-  if [ "z$relCert" == "z" ]; then exit 0; fi
+  if [ -z "$cert_idx" ]; then
+    echo "Package cert index not found; skipping signature check..."
+    exit 0
+  fi
 
-  grep "cert index=\"$relCert\"" /data/system/packages.xml | grep -q `cat /tmp/releasekey`
-  if [ $? -ne 0 ]; then
+  varname="key_$cert_idx"
+  eval "pkgkey=\$$varname"
+
+  if [ "$pkgkey" != "$relkey" ]; then
      echo "You have an installed system that isn't signed with this build's key, aborting..."
      exit 124
   fi
