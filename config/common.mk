ffdd801b47c5373799cd1d46d6911d7605407dcf
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-02-01 13:58:16 -0800

    config: common.mk: Remove deprecated WhisperPush
    WISP-41
    
    Change-Id: Ie03ecd8cbbe8916e8b648e17ac802692668de3ba
    (cherry picked from commit dae684ea8483aa548030a82c649bbef73c3f46d0)

--- config/common.mk
+++ config/common.mk
@@ -163,8 +163,7 @@ PRODUCT_PACKAGES += \
     LockClock \
     CMUpdater \
     CMFota \
-    CMAccount \
-    WhisperPush
+    CMAccount
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
