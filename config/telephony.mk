83c67af13f198bb90c7a9bf2a39add9f134af756
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2016-02-01 22:42:10 +0100

    config: telephony.mk: Remove deprecated WhisperPush
    Depends on http://review.cyanogenmod.org/#/c/129013/
    
    WISP-41
    Change-Id: I5fb95d983d7b29a3e9ca2dd61becb01231c64c3f
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
    (cherry picked from commit 10c9e172272b81c593c9f28861d637330c9f3e81)

--- config/telephony.mk
+++ config/telephony.mk
@@ -10,8 +10,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Mms \
     Stk \
-    CellBroadcastReceiver \
-    WhisperPush
+    CellBroadcastReceiver
 
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
