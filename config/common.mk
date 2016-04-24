e457e5b1f9f51e519bbb13828830cb4ff2be6528
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-04-24 20:06:38 +0300

    cm: Build OpenWeatherMapProvider
    Change-Id: I252ffb33f1c2345931a8b404503e2c923256e251

--- config/common.mk
+++ config/common.mk
@@ -156,7 +156,8 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     ExactCalculator \
     LiveLockScreenService \
-    WeatherProvider
+    WeatherProvider \
+    OpenWeatherMapProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
