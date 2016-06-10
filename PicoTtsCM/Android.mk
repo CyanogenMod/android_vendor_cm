2ed124a312f8b5f4e963f66a2c7a7cb1d8fa6731
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-06-10 10:59:27 -0700

    Override PicoTts with our custom one
    Change-Id: Id115c3141718309da86115d8ebeacf05bf9027b3

---
+++ PicoTtsCM/Android.mk
@@ -0,0 +1,18 @@
+# SVOX Pico TTS Engine
+# This makefile builds both an activity and a shared library.
+LOCAL_PATH := external/svox/pico/
+include $(CLEAR_VARS)
+LOCAL_PACKAGE_NAME := PicoTtsCM
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_OVERRIDES_PACKAGES := PicoTts
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-java-files-under, compat)
+LOCAL_JNI_SHARED_LIBRARIES := libttscompat libttspico
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+include $(BUILD_PACKAGE)
+include $(LOCAL_PATH)/compat/jni/Android.mk \
+    $(LOCAL_PATH)/lib/Android.mk \
+    $(LOCAL_PATH)/tts/Android.mk
