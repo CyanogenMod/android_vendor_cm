d6f59796d4dfd929b323c62a0c1a7cfa7b55aaf4
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-06-10 10:59:27 -0700

    Override PicoTts with our custom one
    See also https://android-review.googlesource.com/#/c/114045/
    
    Due to a stupid DMCA, get creative with a solution
    
    Change-Id: Id115c3141718309da86115d8ebeacf05bf9027b3

---
+++ PicoTtsCM/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+pico_dir := ../../../external/svox/pico
+
+LOCAL_PACKAGE_NAME := PicoTtsCM
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_OVERRIDES_PACKAGES := PicoTts
+
+src_files :\
+    $(pico_dir)/src \
+    $(pico_dir)/compat
+
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, $(src_files))
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/$(pico_dir)/res
+
+LOCAL_JNI_SHARED_LIBRARIES := libttscompat libttspico
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
