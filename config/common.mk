d47096fea11cb049cceabcdab7fd80355292c6e8
niks255 <niks255@mail.ru>
2016-07-08 22:52:38 +0600

    Remove terminal emulator from build
    Change-Id: I080d72178a24e573dd30cc04591fe118ca4f05cc

--- config/common.mk
+++ config/common.mk
@@ -216,12 +216,6 @@ PRODUCT_PACKAGES += \
     Superuser \
     su
 
-# Terminal Emulator
-PRODUCT_COPY_FILES +=  \
-    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 else
