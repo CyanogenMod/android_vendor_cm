3dd46e29b423e1adb9d2480d40b6fb2d4d9d5119
SpotTech <spottechmail@gmail.com>
2016-07-11 09:37:51 +0200

    Track my cm targets
    Track my cm targets for user build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-13.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
+for combo in $(curl -s https://gist.github.com/spottech/47d7433a0f124622ce77f9e6dfb34fd8 | sed -e 's/#.*$//' | grep cm-13.0 | awk '{printf "cm_%s-%s\n", $1, $2}')
 do
     add_lunch_combo $combo
 done
