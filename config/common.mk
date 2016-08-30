14033bacb4e4b1509a8edeaacdca337bfc3e7294
Steve Kondik <steve@cyngn.com>
2016-08-29 18:23:03 -0700

    vendor: fix merge fail
    Change-Id: Iefbb4277c447052fcb37c870e1a79ce29b92ccf1

--- config/common.mk
+++ config/common.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     ExactCalculator \
     LiveLockScreenService \
-    WeatherProvider
+    WeatherProvider \
+    DataUsageProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
