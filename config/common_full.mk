f34ccc6a7d080db78d232d212d6e1fbce3bcf59a
Arne Coucheron <arco68@gmail.com>
2016-09-08 05:40:42 +0200

    cm: Minor config reorganization
     * Add extra tools to mini config. These are needed for CMFM.
     * Add LiveWallpapersPicker, SoundRecorder and Screencast to
       mini config. These apps are extremely small, and only takes
       up around 0.5MB combined.
     * Remove obsolete live wallpapers.
     * Move sound property overrides to common.mk. There's no need
       to have them spread across several files.
    
    Change-Id: If308506cd9cd0a75014de988c242d700db5d16f3

--- config/common_full.mk
+++ config/common_full.mk
@@ -8,16 +8,9 @@ include vendor/cm/config/cm_audio.mk
 
 # Optional CM packages
 PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
     PhotoTable \
     SoundRecorder \
-    PhotoPhase \
     Screencast
 
 # Extra tools in CM
