8e9d811628bdc50b9467a6b2b348dc4aff1eb8dd
Arne Coucheron <arco68@gmail.com>
2016-09-08 02:08:06 +0200

    cm: Refactor mini config
     * Add extra tools. These are needed for CMFM.
     * Move sound property overrides to common_mini_phone.mk
       This makes it consistent with common_full_phone.mk
    
    Change-Id: If308506cd9cd0a75014de988c242d700db5d16f3

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -4,6 +4,11 @@ $(call inherit-product, vendor/cm/config/common_mini.mk)
 PRODUCT_PACKAGES += \
     LatinIME
 
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cm/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
