2677bd7049c0528563373c01eda1670e52506e0f
Griffin Millender <griffinn.millender@gmail.com>
2016-10-07 18:16:06 -0500

    cm: Rework boot animation generation
    Keep the largest bootanimation extracted
    and resize it at build-time.
    
    Change-Id: Iee32f18440ff955d13ed85b273e97cbd540721fc

---
+++ bootanimation/generate-bootanimation.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+CWD=`pwd`
+WIDTH=$1
+RWIDTH=$WIDTH
+HALF_RES=$2
+if [ "$HALF_RES" = "true" ]; then
+    WIDTH=`expr $WIDTH / 2`
+fi
+
+if [ ! -f "/usr/bin/convert" ]; then
+echo "****************************************************"
+echo "The boot animation could not be generated as"
+echo "ImageMagick is not installed in your system."
+echo ""
+echo "Please install ImageMagick from this website:"
+echo "https://imagemagick.org/script/binary-releases.php"
+echo "****************************************************"
+exit
+fi
+
+if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
+    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+else
+RESOLUTION=""$WIDTH"x"$WIDTH""
+
+mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..2}
+tar xvfp "$PWD/vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
+# Create desc.txt
+echo "$RWIDTH" "$RWIDTH" 30 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+cat "$PWD/vendor/cm/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+
+# Create bootanimation.zip
+cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
+
+if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
+mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
+fi
+
+zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
+echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+
+fi
