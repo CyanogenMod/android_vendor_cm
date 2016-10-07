c4642bda82cf52f1c2ddb54906610480d7584383
Griffin Millender <griffinn.millender@gmail.com>
2016-10-07 18:16:06 -0500

    vendor: Rework bootanimation handling
    * We'll compress these at compile rather than shipping zips
    * This is cleaner and easier to maintain than the current
      solution of copying/maintaining prebuilt zip files
    
    Change-Id: I6c504a9b526f270ad5441fc920f5ab157bca5f38
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,38 +1,5 @@
 PRODUCT_BRAND ?= cyanogenmod
 
-ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-# determine the smaller dimension
-TARGET_BOOTANIMATION_SIZE := $(shell \
-  if [ "$(TARGET_SCREEN_WIDTH)" -lt "$(TARGET_SCREEN_HEIGHT)" ]; then \
-    echo $(TARGET_SCREEN_WIDTH); \
-  else \
-    echo $(TARGET_SCREEN_HEIGHT); \
-  fi )
-
-# get a sorted list of the sizes
-bootanimation_sizes := $(subst .zip,, $(shell ls vendor/cm/prebuilt/common/bootanimation))
-bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
-
-# find the appropriate size and set
-define check_and_set_bootanimation
-$(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
-    if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
-      echo $(1); \
-      exit 0; \
-    fi;
-  fi;
-  echo $(TARGET_BOOTANIMATION_NAME); ))
-endef
-$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
-
-ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip
-else
-PRODUCT_BOOTANIMATION := vendor/cm/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
-endif
-endif
-
 ifdef CM_NIGHTLY
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.rommanager.developerid=cyanogenmodnightly
@@ -143,6 +110,7 @@ endif
 PRODUCT_PACKAGES += \
     BluetoothExt \
     CMAudioService \
+    CMBootAnimation \
     CMParts \
     Development \
     Profiles \
