f7afcca4287f41bbe419f5f49284adc00484fd2f
Griffin Millender <griffinn.millender@gmail.com>
2016-10-07 18:16:06 -0500

    vendor: Rework bootanimation handling
    * We'll compress these at compile rather than shipping zips
    * This is cleaner and easier to maintain than the current
      solution of copying/maintaining prebuilt zip files
    
    Change-Id: I6c504a9b526f270ad5441fc920f5ab157bca5f38
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

---
+++ bootanimation/generate-bootanimation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CWD=`pwd`
+WIDTH=$1
+HEIGHT=$(echo "$WIDTH/1.6" | bc)
+HALF_RES=$2
+if [ "$HALF_RES" = "true" ]; then
+    WIDTH=`expr $WIDTH / 2`
+    HEIGHT=`expr $HEIGHT / 2`
+fi
+
+if [ -f "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" ]; then
+    echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+else
+RESOLUTION=""$WIDTH"x"$HEIGHT""
+set >blah
+
+mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part{0..4}
+tar xvfp "$PWD/vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/\$TAR_FILENAME\""
+# create desc.txt
+echo "$WIDTH" "$HEIGHT" 60 > "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+cat "$PWD/vendor/cm/bootanimation/desc.txt" >> "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/desc.txt"
+
+# create bootanimation.zip
+cd "$ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation"
+
+if [ ! -d "$ANDROID_PRODUCT_OUT/system/media" ]; then
+mkdir -p "$ANDROID_PRODUCT_OUT/system/media"
+fi
+
+zip -r0 "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip" .
+echo "$ANDROID_PRODUCT_OUT/system/media/bootanimation.zip"
+
+fi
+
