409bd6084682e20e8d245dff3acb68e919584d53
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-30 09:24:38 +0800

    cm: device: Add initial board & product components
    Change-Id: Iaafd4865e233d18ad53b10dd4efea2d06d57d52f

---
+++ device/product/common/fingerprint.mk
@@ -0,0 +1,7 @@
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
