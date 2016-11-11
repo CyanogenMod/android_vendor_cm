4116588327808228a828c34bcd4eafa90a2ce294
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-10 21:21:07 -0600

    [2/2] cm: allow devices to specify they have a separate /cache partition
    * Set TARGET_USES_SEPARATE_CACHE_PARTITION := true on a device that has a
      physical /cache partition. This, in turn sets a prop that init will use
      to decide what to set the environment variable DOWNLOAD_CACHE to.
    
    * Default to /data/cache per AOSP
    
    Change-Id: I838672ca4638c9665333c1a67257fc435f33f388

--- config/BoardConfigCM.mk
+++ config/BoardConfigCM.mk
@@ -2,3 +2,11 @@
 ifneq ($(WITH_CM_CHARGER),false)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.cm
 endif
+
+ifeq ($(TARGET_USES_SEPARATE_CACHE_PARTITION),true)
+  ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.device.has.cache.partition=1
+else
+  ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.device.has.cache.partition=0
+endif
