6fb778469b7f074bd77998eac6afe9e46b8a15cf
Okeys <Olegrus1100@gmail.com>
2016-11-21 13:41:33 +0300

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -52,7 +52,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+    ro.build.selinux=0
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
