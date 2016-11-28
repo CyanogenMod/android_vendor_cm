edc6aede89ed5090378f32fc069daa399897303c
Okeys <Olegrus1100@gmail.com>
2016-11-28 16:31:38 +0300

    Google Keyboard

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,10 +1,6 @@
 # Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common_full.mk)
 
-# Required CM packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 # Include CM LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/cm/overlay/dictionaries
 
