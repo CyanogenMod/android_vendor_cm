2b670c5aed18a331afa40d945dc47a05fc5523bf
Okeys <Olegrus1100@gmail.com>
2016-11-28 16:32:06 +0300

    Google Keyboard

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -1,9 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_mini.mk)
 
-# Required CM packages
-PRODUCT_PACKAGES += \
-    LatinIME
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cm/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
