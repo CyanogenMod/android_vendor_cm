b1332c15607b907ffdf5b2dcfa2c136cd31039d4
Jorge Martín <jorgemartinespinosa@gmail.com>
2016-12-04 20:08:37 +0100

    Generate bootanimation.zip using the right version of 'tar' on MacOS
    Change-Id: I46d32ea2369cf402b88e0316c6c619743978b704

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -23,7 +23,13 @@ for part_cnt in 0 1 2
 do
     mkdir -p $ANDROID_PRODUCT_OUT/obj/BOOTANIMATION/bootanimation/part$part_cnt
 done
-tar xfp "vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
+HOST_OS=$(uname)
+TAR="tar"
+if [ $HOST_OS = "Darwin" ]; then
+    TAR="gtar"
+fi
+$TAR xfp "vendor/cm/bootanimation/bootanimation.tar" --to-command="convert - -resize '$RESOLUTION' \"png8:$OUT/bootanimation/\$TAR_FILENAME\""
+
 
 # Create desc.txt
 echo "$SIZE" "$SIZE" 30 > "$OUT/bootanimation/desc.txt"
