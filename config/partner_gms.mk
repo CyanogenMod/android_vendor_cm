919c99bd7ea96375945b26ec037eddac1020624e
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-12 23:51:23 +0800

    [DO NOT MERGE] cm: Allow builds with OpenGApps
    Change-Id: I05d867b45cf164209df9ef084a34769cd236e68d

--- config/partner_gms.mk
+++ config/partner_gms.mk
@@ -1,3 +1,6 @@
 ifeq ($(WITH_GMS),true)
 $(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
+else
+GAPPS_VARIANT := pico
+$(call inherit-product-if-exists, vendor/google/build/opengapps-packages.mk)
 endif
